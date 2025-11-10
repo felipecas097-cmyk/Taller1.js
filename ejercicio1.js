fe39bab76db60af0cedc5f6cf76327e866d719c6
felipe cardenas <felipecas097@gmail.com>
2025-11-09 20:23:04 -0500

    ejercicio1 completado

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+// Pedir al usuario el primer número
+let numero1 = prompt("Ingresa el primer número:");
+
+// Pedir al usuario el segundo número
+let numero2 = prompt("Ingresa el segundo número:");
+
+// Convertir los valores a números 
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+// Calcular la suma
+let suma = numero1 + numero2;
+
+// Imprimir la suma en la consola
+console.log("La suma de los dos números es: " + suma);
