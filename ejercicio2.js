9327bb69153e41e2bde1586cccac189e1051fcb5
felipe cardenas <felipecas097@gmail.com>
2025-11-09 21:06:59 -0500

    ejercicio2 completado

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+// pedir el nombre  
+let nombre = prompt("cual es tu nombre");
+
+// pedir el año de nacimiento 
+let anioNacimiento = Number(prompt ("¿en que año naciste?"));
+
+// guardo el año actual
+let anioActual = 2025;
+
+// calculo de la edad
+let edad = anioActual - anioNacimiento;
+
+console.log ("hola " + nombre + ", grandioso! Tienes 20 años.");
+
